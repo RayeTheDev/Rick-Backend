696c42b4f0bfd8ba9abf7fef9b45a119420e3ab6
TemuugenTaleyTimmy <nesthoptemuugen@gmail.com>
2023-03-11 14:19:46 +0800

    updated by tim

--- index.js
+++ index.js
@@ -4,24 +4,10 @@ const express = require("express"),
   cors = require("cors"),
   { connect } = require("./config/db");
 
-<<<<<<< HEAD
-const { articleRouter } = require("./routes/articleRoutes"),
-  { commentRouter } = require("./routes/commentRoutes"),
-  { userRouter } = require("./routes/userRoutes");
-
-const app = express().use(
-    cors(),
-    express.json(),
-    userRouter,
-    articleRouter,
-    commentRouter
-  ),
-=======
 const { articleRouter } = require("./routes/articleRoutes"), { commentRouter } = require("./routes/commentRoutes"), { userRouter } = require("./routes/userRoutes");
 const { categoryRouter } = require("./routes/categoryRoutes");
 
 const app = express().use(cors(), express.json(), userRouter, articleRouter, commentRouter, categoryRouter),
->>>>>>> 39bdbcaa99008eb53875e2ef05917a5eab264153
   port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
